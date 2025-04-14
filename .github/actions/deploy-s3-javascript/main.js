368504807e90e8ecb6fe10e9b998476980a9d349
darkwhiteknight <darkwhiteknightspring@gmail.com>
2025-04-14 12:55:47 +0530

    added javascript custom action to deploy code on aws

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,17 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+// const github = require("@actions/github"); get more github context variables
 const exec = require("@actions/exec");
 
 function run() {
+    const bucket = core.getInput('bucket',{required:true});
+    const bucketRegion = core.getInput('bucket-region',{required:true});
+    const distFolder = core.getInput('dist-folder',{required:true});
+
+    // upload file (using exec command on github aws cli)
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
+
     core.notice("Hello from my custom Javascript Action!")
 }
 
