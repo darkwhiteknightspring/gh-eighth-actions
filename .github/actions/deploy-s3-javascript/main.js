9ba41e3462e4461c55dede8e3fc51463d1f6e5f5
darkwhiteknight <darkwhiteknightspring@gmail.com>
2025-04-14 15:43:32 +0530

    refreshed bucket details

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,7 @@ const core = require("@actions/core");
 const exec = require("@actions/exec");
 
 function run() {
+    // info about the bucket
     const bucket = core.getInput('bucket',{required:true});
     const bucketRegion = core.getInput('bucket-region',{required:true});
     const distFolder = core.getInput('dist-folder',{required:true});
