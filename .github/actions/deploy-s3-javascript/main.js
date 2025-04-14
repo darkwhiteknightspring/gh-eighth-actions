a8cc7ab24753e7d36d714a872676f561c6024125
darkwhiteknight <darkwhiteknightspring@gmail.com>
2025-04-14 16:00:18 +0530

    generating the url of the website

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,7 @@ const core = require("@actions/core");
 const exec = require("@actions/exec");
 
 function run() {
-    // info about the bucket
+    // info about the bucketRegion
     const bucket = core.getInput('bucket',{required:true});
     const bucketRegion = core.getInput('bucket-region',{required:true});
     const distFolder = core.getInput('dist-folder',{required:true});
@@ -12,8 +12,8 @@ function run() {
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
-
-    core.notice("Hello from my custom Javascript Action!")
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url',websiteUrl);
 }
 
 run();
